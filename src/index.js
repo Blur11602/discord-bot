1e1e321790dc04775df2cd152dc1f9066f746df5
Blur11602 <ohcsteer@gmail.com>
2022-12-15 09:10:43 +1100

    new command and changed command
    Added 'ephemeral', meaning only the user that used the command can see the message (on specific commands).
    
    Added 'activedevbadge' command which replies with a link on how to claim your badge and that you will now be able to in 24hrs.
    
    Split 'user' command to not include token, instead it has its own command ('token') with a warning that no one can do anything with just the start of your token.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // @note: USE COMMAND 'npm run dev' IN POWERSHELL TO START THE BOT
-// @note: https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-interactionCreate
+// @note: interaction.reply({content: "Only you can see this!", ephemeral: true });
 import { config } from "dotenv";
 import { Client, GatewayIntentBits, InteractionResponse, Routes, time, inlineCode, codeBlock, blockQuote } from "discord.js";
 import { REST } from "@discordjs/rest";
@@ -27,6 +27,9 @@ client.on("ready", () => {
 client.on("interactionCreate", (interaction) => { // https://discord.js.org/#/docs/discord.js/main/class/CommandInteraction
 	if (!interaction.isCommand()) return;
 	switch (interaction.commandName) {
+		case "activedevbadge":
+			interaction.reply({content: "Command run successfully.\nPlease wait up to 24hrs, then go to https://discord.com/developers/active-developer to claim your badge!", ephemeral: true });
+			break;
 		case "test":
 			interaction.reply(`**<@${interaction.user.id}>**, I am sentient and I understand the meaning of life\n *- Locale: ${interaction.locale} | Time: ${time(interaction.createdAt, "T")}*`);
 			//interaction.reply(inlineCode(`Interaction: ${interaction.id} | User: ${interaction.user.id}`));
@@ -41,8 +44,10 @@ client.on("interactionCreate", (interaction) => { // https://discord.js.org/#/do
 			interaction.reply(`Pong!\n${codeBlock("Client: " + client.ws.ping + "ms\nAPI: " + (Date.now() - interaction.createdTimestamp) + "ms")}`);
 			break;
 		case "user":
-			interaction.reply(`User: ${interaction.user.tag}\nID: ${interaction.user.id}\nJoined: ${time(interaction.user.createdAt, "F")}\nToken Start: ${btoa(interaction.user.id)}\nAvatar: ${interaction.user.avatarURL()}`);
+			interaction.reply(`User: ${interaction.user.tag}\nID: ${interaction.user.id}\nJoined: ${time(interaction.user.createdAt, "F")}\nAvatar: ${interaction.user.avatarURL()}`);
 			break;
+		case "token":
+			interaction.reply({ content: `Start of your token: ${btoa(interaction.user.id)}\nIf anyone sends you this, do not worry.\nIt is simply your user id encoded in base64.`, ephemeral: true });
 		default:
 			break;
 	}
@@ -52,6 +57,9 @@ client.on("interactionCreate", (interaction) => { // https://discord.js.org/#/do
 async function main() {
 	const commands = [
 		{
+			name: "activedevbadge",
+			description: "Replies to only you with a message to claim your Active Developer badge"
+		}, {
 			name: "test",
 			description: "Replies with '*name*, I am sentient.' if the bot is working",
 		}, {
@@ -74,7 +82,10 @@ async function main() {
 		}, {
 			name: "user",
 			description: "Replies with your user information",
-		},
+		}, {
+			name: "token",
+			description: "Replies to only you with the start of your token"
+		}
 	];
 
 	try {
